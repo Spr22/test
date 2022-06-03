7f879343bbee8c35b674bf334a727659963e4805
Spr22 <88624599+Spr22@users.noreply.github.com>
2022-06-03 22:04:16 +0530

    Created using Colaboratory

---
+++ testfirst.py
@@ -0,0 +1,68 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "testfirst.ipynb",
+      "provenance": [],
+      "collapsed_sections": [],
+      "authorship_tag": "ABX9TyN8eYfYFGj+Rgs87jcgoJwr",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Spr22/test/blob/main/testfirst.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "GvFd5jJoOHBn",
+        "outputId": "650ef7bb-16ef-4f13-b60e-d77361cba66b"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "what is your name: swiggy\n",
+            "swiggy\n"
+          ]
+        }
+      ],
+      "source": [
+        "name = input (\"what is your name: \")\n",
+        "print (name)"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        ""
+      ],
+      "metadata": {
+        "id": "YRrrsnNDTy7i"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
